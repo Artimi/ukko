6f695d7852a3f2c2f7c056ef2101ee811636da84
Petr Šebek <petrsebek1@gmail.com>
2014-10-25 16:26:01 +0200

    Command line arguments

--- ukko/ukko.py
+++ ukko/ukko.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+import argparse
+from rcpparser import RCPParser
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='RCPSP Solver')
+    parser.add_argument('rcp_file', help='RCP formated file with problem description')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    rcpparser = RCPParser()
+    problem_dict = rcpparser(args.rcp_file)
+    print problem_dict
+
+
+if __name__ == '__main__':
+    main()
